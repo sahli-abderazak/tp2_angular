92bcfc91d16e10a1833d01ffe2350b843d07811e
sahli-abderazak <sahliabderazak53@gmail.com>
2023-10-07 23:06:31 +0100

    tp2

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,25 @@
+import { Component,OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html',
+  
+})
+export class AddProduitComponent implements OnInit {
+
+  newProduit = new Produit();
+
+  message : String ='';
+
+  constructor(private ProduitService : ProduitService){}
+  ngOnInit(): void {
+    
+  }
+  addProduit(){
+    // console.log(this.newProduit);
+    this.ProduitService.ajouterProduit(this.newProduit);
+    this.message="produit "+this.newProduit.nomProduit +"ajouter avec succes ! ";
+    }
+    
+}
---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,26 @@
+import { Component,OnInit} from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+
+})
+export class ProduitsComponent implements OnInit{
+
+  produits? : Produit[];
+  
+  // private ProduitService= new ProduitService();
+  constructor(private ProduitService:ProduitService) {
+    // this.produits=[];
+    // this.produits = ProduitService.listeProduit();
+    }
+
+    ngOnInit():void{
+      this.produits = this.ProduitService.listeProduit();
+    }
+}
+
+
